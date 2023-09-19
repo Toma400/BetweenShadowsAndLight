469c43644c7aca73c787ecf8aba39a00b9575b17
Toma400 <thomasso_calgori@o2.pl>
2023-09-19 17:14:05 +0200

    Settings menu built

--- bsal.py
+++ bsal.py
@@ -1,8 +1,31 @@
 from utils import *
 
-print(f"""
-[1] {langstr("menu__start")}
-[2] {langstr("menu__load")}
-[3] {langstr("menu__settings")}
-[4] {langstr("menu__quit")}
-""")
+current_menu = 0 # menu
+end = [] # add stuff here to exit the game
+
+while not end:
+    if current_menu == 0:
+        print(f"""
+        [1] {langstr("menu__start")}
+        [2] {langstr("menu__load")}
+        [3] {langstr("menu__settings")}
+        [4] {langstr("menu__quit")}
+        """)
+        choice = input("")
+        match choice:
+            case "1": pass
+            case "2": pass
+            case "3": current_menu = 3
+            case "4": end.append("")
+    elif current_menu == 3:
+        print(f"""
+        {langstr("menu__langcur")} {langstr("menu__lang")}
+        {langstr("menu__langav")}
+        """)
+        for lange in lang_list:
+            print(f"- {lange}")
+        choice = input("")
+        if choice != "q":
+            langswitch(choice)
+        else:
+            current_menu = 0
--- utils.py
+++ utils.py
@@ -1,12 +1,18 @@
 import toml
 import os
 
-settings  = toml.load("settings.toml")
-lang_list = [f for (_, _, f) in os.walk("lang")]
-languages = {l.replace(".toml", ""): toml.load(f"lang/{l}") for l in lang_list[0]}
+settings     = toml.load("settings.toml")
+lang_list    = [f2.replace(".toml", "") for f2 in [f for (_, _, f) in os.walk("lang")][0]]
+languages    = {l: toml.load(f"lang/{l}.toml") for l in lang_list}
 
-class Settings:
-    lang = settings["language"]
+def current_lang() -> str:
+    return settings["language"]
 
 def langstr(key: str) -> str:
-    return languages[Settings.lang][key]
+    return languages[current_lang()][key]
+
+def langswitch(key: str):
+    if key in lang_list:
+        settings["language"] = key
+    with open("settings.toml", "w") as s:
+        toml.dump(settings, s)
