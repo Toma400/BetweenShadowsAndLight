2c84796b5843aba79115359ec24abe0ee7442122
Toma400 <thomasso_calgori@o2.pl>
2025-12-30 11:38:35 +0100

    Small fixes

--- pack.py
+++ pack.py
@@ -1,5 +1,4 @@
 import zipfile
-import shutil
 import os
 
 FILES = [
@@ -9,11 +8,16 @@ FILES = [
     "settings.toml"
 ]
 
+OUT = "BetweenShadowsAndLight.zip"
+
 os.system("nim c bsal.nim")
 # os.system("rcedit-x64 'Between Shadows and Light.exe' --set-icon 'bsal.ico'")
 # todo: uncomment above when BSaL icon is made
 
-with zipfile.ZipFile("BetweenShadowsAndLight.zip", mode="w") as archive:
+if os.path.exists(OUT):
+    os.remove(OUT)
+
+with zipfile.ZipFile(OUT, mode="w") as archive:
     for f in FILES:
         with archive.open(f, "w") as fw:
             with open(f, "rb") as fr:
